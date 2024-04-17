c4a2988f6b2c22ff6821ecb70d4d2e970b6077e7
gabriel8Programmer <gabriel8github@gmail.com>
2024-04-17 12:48:47 -0300

    change script js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,48 @@
- //get elements
- const btnConvert = document.querySelector("#btn-convert");
- const binaryInput = document.querySelector("#binary-input");
- const result = document.querySelector("#result");
- const error = document.querySelector("#error");
+//get elements
+const btnConvert = document.querySelector("#btn-convert");
+const binaryInput = document.querySelector("#binary-input");
+const result = document.querySelector("#result");
+const error = document.querySelector("#error");
  
- function handleBinaryInput(e) {
-  let inputValue = e.target.value;
-  let timeError;
-  const error = document.getElementById("error");
-  
-  if (inputValue[inputValue.length - 1] !== "0" && inputValue[inputValue.length - 1] !== "1") {
-      // Remove characters
-      inputValue = inputValue.slice(0, -1);
-      e.target.value = inputValue;
-      // Remove class 'hide' from error element
-      error.classList.remove("hide");
-      // Set new timeout for hiding the error message
-      timeError = setTimeout(() => {
-          // Add class 'hide' to error element after 3000ms
-          error.classList.add("hide");
-      }, 3000);
-  }
-  
-  if (inputValue == ""){
-     return;
-  }
-  
-  //set value
-   result.innerText = parseInt(inputValue, 2);
-   //set focus in binaryInput
-   binaryInput.focus();
- }
+//time from each error message
+let timeErrorMessage;
  
- binaryInput.addEventListener("input", handleBinaryInput);
+function validateBinary(binaryNumber){
+  const numbersPossible = [0, 1];
+  return numbersPossible.some(number => number == binaryNumber);
+}
+ 
+function handleBinaryInput(e) {
+  let inputValue = (e.target.value == "")? "0": e.target.value;
+  const error = document.querySelector("#error");
+  
+  //get last caracter in inputValue
+  const lastIndex = inputValue[inputValue.length -1];
+  
+  if (validateBinary(lastIndex)){
+    //hide possible error message
+    error.classList.add("hide");
+    //set value
+    result.innerText = parseInt(inputValue, 2);
+    //set focus in binaryInput
+    binaryInput.focus();
+    return;
+  }
+
+  //clear timeout
+  clearTimeout(timeErrorMessage);
+  
+  //in the error case
+  // Remove characters
+  inputValue = inputValue.slice(0, -1);
+  e.target.value = inputValue;
+  // Remove class 'hide' from error element
+  error.classList.remove("hide");
+  // Set new timeout for hiding the error message
+  timeErrorMessage = setTimeout(() => {
+    // Add class 'hide' to error element after 3000ms
+    error.classList.add("hide");
+  }, 3000);
+}
+
+binaryInput.addEventListener("keyup", handleBinaryInput);
